d04d2f9464fe34099d0f8f0d33193d04de772662
slabada228 <lenovov580cegor@gmail.com>
2024-05-14 12:23:01 +0300

    Commit 3

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,10 +1,11 @@
 pipeline {
-
     agent any
 
     stages{
-        stage("Build"){
-            echo "Builddddddd"
+        stage('Build'){
+            steps{
+                echo 'SLABADA'
+            }
         }
     }
 }
