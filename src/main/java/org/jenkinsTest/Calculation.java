a26945467fbd03a3adc917ff4ecb9263be47a6c9
slabada228 <lenovov580cegor@gmail.com>
2024-05-14 11:28:04 +0300

    Commit 1

---
+++ src/main/java/org/jenkinsTest/Calculation.java
@@ -0,0 +1,8 @@
+package org.jenkinsTest;
+
+public class Calculation {
+
+    public int sum(int a, int b){
+        return a + b;
+    }
+}
---
+++ src/test/java/org/jenkinsTest/CalculationTest.java
@@ -0,0 +1,21 @@
+package org.jenkinsTest;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculationTest {
+
+    @InjectMocks
+    Calculation calculation;
+
+    @Test
+    void sum() {
+        var r = calculation.sum(5,5);
+        assertEquals(10, r);
+    }
+}
